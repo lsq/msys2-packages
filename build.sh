3021fce32739d0a1ec74bf29cc28b564bd889a5f
lsq <lsqypj.gmail.com>
2025-04-27 09:33:46 +0800

    change: git committer info to global

--- build.sh
+++ build.sh
@@ -517,8 +517,10 @@ if [[ "${BASH_SOURCE}" = "${0}" ]]; then
     # echo "${check_version_list[*]}"
     [[ -z "${option_build+true}" ]] && check_update
     [[ ! -z "${option_report+'true'}" ]] && report "Update information" "${updateinfos[@]}"
-    git config --local user.email "github-actions[bot]@users.noreply.github.com"
-    git config --local user.name "github-actions[bot]"
+    # git config --local user.email "github-actions[bot]@users.noreply.github.com"
+    # git config --local user.name "github-actions[bot]"
+    git config --global user.email "github-actions[bot]@users.noreply.github.com"
+    git config --global user.name "github-actions[bot]"
     [[ ! -z "${option_verbose+set}" ]] && declare -p pkginfos
     [[ ! -z "${option_verbose+set}" ]] && declare -p updateinfos
     # echo "jobsinfo=\"$(declare -p updateinfos|sed 's/\\/\\\\/g;s/"/\\"/g')\"" #>> $GITHUB_OUTPUT
